1b1fda63b8550581251488b5848efdc57957febe
abishek-gurappu <99612932+abishek-gurappu@users.noreply.github.com>
2022-02-23 15:55:23 -0600

    Update urls.py

--- evernote/urls.py
+++ evernote/urls.py
@@ -3,7 +3,9 @@ from urllib.parse import urlparse
 from django.urls import URLPattern, path
 from . import views
 
+#the url patterns store all the urls that are being used. here its the main.
 urlpatterns = [
     path('', views.home, name="home"),
+    path('/create', views.create, name="create1")
     
 ]
