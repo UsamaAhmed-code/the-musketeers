4ff2b8e2ae0fb963c208b48b1867b4dc993906fc
DeekshithMattepally <98776814+DeekshithMattepally@users.noreply.github.com>
2022-02-24 12:28:49 -0600

    Update views.py

--- evernote/views.py
+++ evernote/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 
-
-
 from django.http import HttpResponse
 
 from evernote.models import  CountPage
-
-
 def home(request):
 
  count_num = CountPage.objects.get(id = 1)
@@ -14,6 +10,6 @@ def home(request):
  count_num = CountPage.objects.get(id = 1)
  #print(count_num)
 
- return render(request, 'home.html', {'items' : count_num})
+ return render(request,'home.html', {'items' : count_num})
 
 
