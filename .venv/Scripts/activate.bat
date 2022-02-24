57b01e6c89a8f50a355073853cc5299cab2be609
Mounikar99 <98777326+Mounikar99@users.noreply.github.com>
2022-02-23 18:59:17 -0600

    Update activate.bat

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -21,7 +21,13 @@ set PROMPT=(.venv) %PROMPT%
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
 
+if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
+if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(.venv) 
+
+:END
 if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
     set _OLD_CODEPAGE=
