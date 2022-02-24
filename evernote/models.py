5506c97f5c8ffa51667ec41766a5a437f37e280b
Mounikar99 <98777326+Mounikar99@users.noreply.github.com>
2022-02-24 16:56:31 -0600

    Update models.py

--- evernote/models.py
+++ evernote/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class CountPage(models.Model):
     no_of_visits = models.IntegerField()
-   
+   from django.db import models
+
+# Create your models here.
+class Visits(models.Model):
+    no_of_visits=models.IntegerField(max_length=300)
