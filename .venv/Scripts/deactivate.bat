1414caca64a86810fc64ba5152ae07467f98ae2f
Mounikar99 <98777326+Mounikar99@users.noreply.github.com>
2022-02-23 19:00:50 -0600

    Update deactivate.bat

--- .venv/Scripts/deactivate.bat
+++ .venv/Scripts/deactivate.bat
@@ -10,3 +10,13 @@ if defined _OLD_VIRTUAL_PYTHONHOME (
     set _OLD_VIRTUAL_PYTHONHOME=
 )
 
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+)
+
+set _OLD_VIRTUAL_PATH=
+
+set VIRTUAL_ENV=
+set VIRTUAL_ENV_PROMPT=
+
+:END
