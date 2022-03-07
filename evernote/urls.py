9f84ae897bbbc94163b481e46ca40e646ae51a21
UsamaAhmed-code <usamachatha75@gmail.com>
2022-03-06 23:27:02 -0600

    page connectivity code

---
+++ evernote/urls.py
@@ -0,0 +1,9 @@
+from multiprocessing.spawn import import_main_path
+from django.urls import URLPattern, path
+from evernote import views
+
+urlpatterns = [
+   path('', views.loginpage, name= "login"),
+    path('register/', views.registerpage, name= "register"),
+    path('home/', views.home, name= "home")
+]
