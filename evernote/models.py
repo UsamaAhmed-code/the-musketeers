2211c199b195df4426fd05377a504c653b2ce362
UsamaAhmed-code <usamachatha75@gmail.com>
2022-04-06 13:30:44 -0500

    this file contain the the database create code to save the notes. In database notes title, content, creation time and update time will be store

---
+++ evernote/models.py
@@ -0,0 +1,21 @@
+import email
+from lzma import MODE_NORMAL
+from xml.dom.minidom import Document
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class CountPage(models.Model):
+    content = models.IntegerField()
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    content = RichTextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Meta:
+    ordering = ('title', )    
+
